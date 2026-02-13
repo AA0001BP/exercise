af94080079c79b95cbd238c37927b7475dfba668
AA0001BP <taborovy@sherborne.org>
2026-02-13 11:52:00 +0000

    Final testing and minor fixes before submission

--- rango/views.py
+++ rango/views.py
@@ -75,8 +75,6 @@ def add_category(request):
         if form.is_valid():
             form.save(commit=True)
             return redirect('/rango/')
-        else:
-            print(form.errors)
 
     return render(request, 'rango/add_category.html', {'form': form})
 
@@ -105,8 +103,6 @@ def add_page(request, category_name_slug):
                 return redirect(reverse('rango:show_category',
                                         kwargs={'category_name_slug':
                                                 category_name_slug}))
-        else:
-            print(form.errors)
 
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
